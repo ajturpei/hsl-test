3ad10fc9ccf8fd71cb4085fa396f72e756d53fc7
ajturpei <aleksi.turpeinen83@gmail.com>
2021-01-19 00:43:07 +0200

    etest

--- packages/client/src/utils/config.ts
+++ packages/client/src/utils/config.ts
@@ -2,8 +2,7 @@ const config = {
   apiKey: "5e581f69ad9c4225a83ac21d4bf37809",
   graphqlAPI: "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
   mqttVPAPIDev: "ws://localhost:4000/graphql",
-  mqttVPAPIProd:
-    "wss://hslrealtime-env.eba-pqmaifhs.eu-north-1.elasticbeanstalk.com:4000/graphql",
+  mqttVPAPIProd: "wss://aleksit.com/server/graphql",
   mapEndpoint:
     "https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=",
   hslMap: "https://cdn.digitransit.fi/map/v1/hsl-map/{z}/{x}/{y}@2x.png",
